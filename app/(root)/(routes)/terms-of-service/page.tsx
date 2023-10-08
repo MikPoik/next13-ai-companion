77d56c8872f6d2182bbd0890adbe7a97c0840afb
MikPoik <42643139+MikPoik@users.noreply.github.com>
2023-10-08 19:46:31 +0000

    fixed escape chars from terms-service

--- app/(root)/(routes)/terms-of-service/page.tsx
+++ app/(root)/(routes)/terms-of-service/page.tsx
@@ -6,12 +6,12 @@ const TermsofServicePage = async () => {
       <h3 className="text-lg font-medium"><strong>Terms and Conditions</strong></h3>
       <div className="text-muted-foreground text-sm"><br></br>
         <p>Welcome to TruLuv!</p><br></br>
-        <p>These terms and conditions outline the rules and regulations for the use of ParaSocial's Website, located at https://www.truluv.me/.</p><br></br>
+        <p>These terms and conditions outline the rules and regulations for the use of ParaSocial&apos;s Website, located at https://www.truluv.me/.</p><br></br>
         <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use TruLuv if you do not agree to take all of the terms and conditions stated on this page.</p><br></br>
-        <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of us. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p><br></br>
+        <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: &rdquo;Client&rdquo;, &rdquo;You&rdquo; and &rdquo;Your&rdquo; refers to you, the person log on this website and compliant to the Company&apos;s terms and conditions. &rdquo;The Company&rdquo;, &rdquo;Ourselves&rdquo;, &rdquo;We&rdquo;, &rdquo;Our&rdquo; and &rdquo;Us&rdquo;, refers to our Company. &rdquo;Party&rdquo;, &rdquo;Parties&rdquo;, or &rdquo;Us&rdquo;, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client&apos;s needs in respect of provision of the Company&apos;s stated services, in accordance with and subject to, prevailing law of us. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p><br></br>
         <h3 className="text-lg font-medium">Cookies</h3><br></br>
-        <p>We employ the use of cookies. By accessing TruLuv, you agreed to use cookies in agreement with the ParaSocial's Privacy Policy. </p><br></br>
-        <p>Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p><br></br>
+        <p>We employ the use of cookies. By accessing TruLuv, you agreed to use cookies in agreement with the ParaSocial&apos;s Privacy Policy. </p><br></br>
+        <p>Most interactive websites use cookies to let us retrieve the user&apos;s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p><br></br>
         <h3 className="text-lg font-medium">License</h3><br></br>
         <p>Unless otherwise stated, ParaSocial and/or its licensors own the intellectual property rights for all material on TruLuv. All intellectual property rights are reserved. You may access this from TruLuv for your own personal use subjected to restrictions set in these terms and conditions.</p><br></br>
         <p>You must not:</p><br></br>
@@ -41,7 +41,7 @@ const TermsofServicePage = async () => {
           <li>Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and</li>
           <li>System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.</li>
         </ul><br></br>
-        <p>These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party's site.</p><br></br>
+        <p>These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party&apos;s site.</p><br></br>
         <p>We may consider and approve other link requests from the following types of organizations:</p><br></br>
         <ul>
           <li>commonly-known consumer and/or business information sources;</li>
@@ -53,21 +53,21 @@ const TermsofServicePage = async () => {
           <li>educational institutions and trade associations.</li>
         </ul><br></br>
         <p>We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of ParaSocial; and (d) the link is in the context of general resource information.</p><br></br>
-        <p>These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party's site.</p><br></br>
+        <p>These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party&apos;s site.</p><br></br>
         <p>If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to ParaSocial. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.</p><br></br>
         <p>Approved organizations may hyperlink to our Website as follows:</p><br></br>
         <ul>
           <li>By use of our corporate name; or</li>
           <li>By use of the uniform resource locator being linked to; or</li>
-          <li>By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party's site.</li>
+          <li>By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party&apos;s site.</li>
         </ul>
-        <p>No use of ParaSocial's logo or other artwork will be allowed for linking absent a trademark license agreement.</p><br></br>
+        <p>No use of ParaSocial&apos;s logo or other artwork will be allowed for linking absent a trademark license agreement.</p><br></br>
         <h3 className="text-lg font-medium">iFrames</h3><br></br>
         <p>Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.</p><br></br>
         <h3 className="text-lg font-medium">Content Liability</h3><br></br>
         <p>We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.</p><br></br>
         <h3 className="text-lg font-medium">Reservation of Rights</h3><br></br>
-        <p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it's linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p><br></br>
+        <p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it&apos;s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p><br></br>
         <h3 className="text-lg font-medium">Removal of links from our website</h3><br></br>
         <p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.</p>
         <p>We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.</p><br></br>
