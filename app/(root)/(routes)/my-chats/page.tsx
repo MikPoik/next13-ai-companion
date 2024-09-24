bab9f190971f21c570d1d7bcfe8183b13ae6a877
MikPoik <42643139+MikPoik@users.noreply.github.com>
2024-09-24 05:31:02 +0000

    Add logged in check to my-chat

--- app/(root)/(routes)/my-chats/page.tsx
+++ app/(root)/(routes)/my-chats/page.tsx
@@ -1,6 +1,12 @@
 import { MyChats } from "./components/my-chats";
+import { auth, currentUser, } from "@clerk/nextjs/server";
 
 const MyChatsPage = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+      return auth().redirectToSignIn();
+  }
   return (
     <div className="h-full p-4 space-y-2">
       <h2 className="text-2xl font-bold text-center">My chats</h2>
